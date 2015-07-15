ad32d54e732ca71bd9029c7c6fea53afe20bf61b
Alan R <alanrandell@gmail.com>
2015-07-14 19:32:44 -0700

    Added camera buttons and UIImagePickerController

---
+++ FeedCell.swift
@@ -0,0 +1,17 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Alan Randell-Chen on 7/14/15.
+//  Copyright (c) 2015 Alan Randell-Chen. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+}
