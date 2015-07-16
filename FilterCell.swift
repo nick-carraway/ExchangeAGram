85eb9a75d4f1b3658843d408835468d711e4814b
Alan R <alanrandell@gmail.com>
2015-07-16 11:41:41 -0700

    Filter Cell

---
+++ FilterCell.swift
@@ -0,0 +1,26 @@
+//
+//  FilterCell.swift
+//  ExchangeAGram
+//
+//  Created by Alan Randell-Chen on 7/16/15.
+//  Copyright (c) 2015 Alan Randell-Chen. All rights reserved.
+//
+
+import UIKit
+
+class FilterCell: UICollectionViewCell {
+    var imageView:UIImageView!
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
+        
+        contentView.addSubview(imageView)
+        
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
